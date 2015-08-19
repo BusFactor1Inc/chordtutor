61e06e234cb62d3e62ae84fd4cbf03760508050e
Burton Samograd <burton.samograd@gmail.com>
2015-08-19 12:32:36 -0600

    Volume controls.

--- client/volume-view.js
+++ client/volume-view.js
@@ -1,7 +1,56 @@
-var VolumeView = View({
-    type: 'VolumeView',
-    render: function () {
-        return this.$el.html('VolumeView');
+var VolumeUpButtonView = View({
+    type: 'TinyButton VolumeUpButtonView',
+    events: {
+        'click': function () {
+            alert('Volume Up');
+        }
     }
 });
 
+var VolumeDownButtonView = View({
+    type: 'TinyButton VolumeDownButtonView',
+    events: {
+        'click': function () {
+            alert('Volume Down');
+        }
+    }
+});
+
+var VolumeControlsView = View({
+    type: 'VolumeControlsView',
+    init: function (model) {
+        this.create('up', new VolumeUpButtonView(model));
+        this.create('down', new VolumeDownButtonView(model));
+    },
+    render: function () {
+        var html = [
+            this.up().$el,
+            this.down().$el,
+        ];
+        return this.$el.html(html);
+    }
+});
+
+var VolumeToggleButtonView = View({
+    type: 'LargeButton VolumeToggleButtonView',
+    events: {
+        'click': function () {
+            alert('Volume Toggle');
+        }
+    }
+});
+
+var VolumeView = View({
+    type: 'VolumeView',
+    init: function (model) {
+        this.create('toggle', new VolumeToggleButtonView(model));
+        this.create('controls', new VolumeControlsView(model));
+    },
+    render: function () {
+        var html = [
+            this.toggle().$el,
+            this.controls().$el
+        ];
+        return this.$el.html(html);
+    }
+});
