8f00c62d894edcb1895fc05d90713de539c493e7
Krishna Kalvai <kishmu@gmail.com>
2015-08-20 16:21:00 -0700

    saving to file storage

--- client/models.js
+++ client/models.js
@@ -308,8 +308,63 @@ var App = Model({
             self.set('tempo', Number(songInfo.beatsPerMinute()));
             self.player().songInfo(songInfo);
             next(songInfo);
+            self.saveToLocalStorage(file.name, contents);
         };
         reader.readAsText(file);
+    },
+
+    saveToLocalStorage: function (filename, contents) {
+        var onInitFs = function (fs) {
+            fs.root.getDirectory('chordtutor', {create: true}, function (dirEntry) {
+                dirEntry.getFile(filename, {create: true, exclusive: true}, function (fileEntry) {
+                    // Create a FileWriter object for our FileEntry (log.txt).
+                    fileEntry.createWriter(function (fileWriter) {
+
+                      fileWriter.onwriteend = function(e) {
+                        console.log('Write completed.');
+                      };
+
+                      fileWriter.onerror = function(e) {
+                        console.log('Write failed: ' + e.toString());
+                      };
+
+                      // Create a new Blob and write it to log.txt.
+                      var blob = new Blob([contents], {type: 'text/plain'});
+
+                      fileWriter.write(blob);
+
+                    }, errorHandler);  
+                }, errorHandler);
+            }, errorHandler); 
+        };
+
+        var errorHandler = function (e) {
+          var msg = '';
+          switch (e.message) {
+            case FileError.QUOTA_EXCEEDED_ERR:
+              msg = 'QUOTA_EXCEEDED_ERR';
+              break;
+            case FileError.NOT_FOUND_ERR:
+              msg = 'NOT_FOUND_ERR';
+              break;
+            case FileError.SECURITY_ERR:
+              msg = 'SECURITY_ERR';
+              break;
+            case FileError.INVALID_MODIFICATION_ERR:
+              msg = 'INVALID_MODIFICATION_ERR';
+              break;
+            case FileError.INVALID_STATE_ERR:
+              msg = 'INVALID_STATE_ERR';
+              break;
+            default:
+              msg = 'Unknown Error';
+              break;
+          };
+          console.log('Error: ' + msg);
+        };
+
+        window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+        window.requestFileSystem(window.TEMPORARY, 1024*1024, onInitFs, errorHandler);
     },
 
     loadDefaultSong: function (next) {
