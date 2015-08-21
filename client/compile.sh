ae65534bd719ef27c1bf3232a0129f4799f6443a
Michaela Pruneau-Campbell <michaela.pruneau-campbell@autodesk.com>
2015-08-21 10:03:04 -0700

    Adding meta tag for additional responsive sauce & small tweak to css for smaller screens.

--- client/compile.sh
+++ client/compile.sh
@@ -1,5 +1,5 @@
 cat <<EOF
-<!DOCTYPE html><html><head><title>Song Tutor</title><meta charset='UTF-8'><style>
+<!DOCTYPE html><html><head><title>Chord Tutor</title><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><style>
 $(cat style.min.css)
 </style><script>
 $(cat app.min.js)
